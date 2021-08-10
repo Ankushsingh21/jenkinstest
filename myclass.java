695ce8650b395074c4b7d3eded1361f9c023a28d
Ankush Singh <43205706+Ankushsingh21@users.noreply.github.com>
2021-08-10 12:24:14 +0530

    Add files via upload
    first commit in this repo

---
+++ myclass.java
@@ -0,0 +1,10 @@
+
+
+public class myclass {
+
+	public static void main(String[] args) {
+		System.out.println("this is Ankush singh ");
+                System.out.println("testing jenkins for the first time  ");
+	}
+
+}
